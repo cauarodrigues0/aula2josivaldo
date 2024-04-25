6e8139cfbabf2aa5dcdf5ed5c901bb63895d8b56
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-25 15:29:34 -0300

    propriedades button e boxlayout com label

---
+++ boxlayout.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class MyApp(App):
+    def build (self):
+        layout = BoxLayout(orientation = "vertical") 
+        self.lab1 = Label(text = "SENAI", color = [1, 0, 0, 1], font_size = 40, bold= True)
+        self.lab2 = Label(text = "SESI", color = [0, 1, 0, 1], font_size = 40, italic = True)
+        self.lab3 = Label(text = "ENEM", color = [0, 0, 1, 1], font_name = "Georgia", underline = True)   
+
+        layout.add_widget(self.lab1)
+        layout.add_widget(self.lab2)
+        layout.add_widget(self.lab3)
+        return layout
+if __name__ == "__main__":
+    MyApp().run()
