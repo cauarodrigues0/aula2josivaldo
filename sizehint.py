4c65d23ce9c59375824f886c05baec114fca42bd
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-25 17:27:19 -0300

    utilizado o sizehint

---
+++ sizehint.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build (self):
+        return Button(text='Hello World', size_hint = (0.2, 0.5))
+if __name__ == '__main__':
+    MyApp().run()
