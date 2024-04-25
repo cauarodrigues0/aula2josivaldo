988ad6ba936c9eb65912f0eec4c040e8c42b3af1
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-25 15:38:56 -0300

    criado a funcionalidade de apertar botão

--- button.py
+++ button.py
@@ -2,9 +2,15 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.utils import get_color_from_hex
 
+def botao_pressionado(instance):
+    print("Botão Pressionado")
+
+
 class MyApp(App):
     def build(self):
-        return Button(text = "Clique Aqui!", background_color = get_color_from_hex("3498db"))
+       btn = Button(text = "Clique Aqui!", background_color = get_color_from_hex("3498db"))
+       btn.bind(on_press = botao_pressionado)
+       return btn
 
 if __name__ == "__main__":
     MyApp().run()
